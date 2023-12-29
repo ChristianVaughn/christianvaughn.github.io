8acf5d144713745ed9d575b2226df0e6269596d7
Christian Vaughn <me@christianvaughn.net>
2023-12-29 11:01:04 -0800

    yeet lets try and get this orange theme a bit lighter.
    might need to undo this because im testing on a color inaccurate monitor

--- pages/components/demo/settingData.ts
+++ pages/components/demo/settingData.ts
@@ -1,60 +1,63 @@
 import { liveURL } from "../../../custom.config";
-import { IPageTheme, IPageURL } from "../../../typescript/interfaces/common.interface";
+import {
+  IPageTheme,
+  IPageURL,
+} from "../../../typescript/interfaces/common.interface";
 
 export const pageURLs: IPageURL[] = [
   {
-    url: liveURL + '/',
-    title: 'Index 1'
-  }
-]
+    url: liveURL + "/",
+    title: "Index 1",
+  },
+];
 
 export const pageThemes: IPageTheme[] = [
   {
-    name: 'orange',
-    color: '#fd7e14'
+    name: "orange",
+    color: "#fdb068",
   },
   {
-    name: 'teal',
-    color: '#22d4b5'
+    name: "teal",
+    color: "#22d4b5",
   },
   {
-    name: 'primary',
-    color: '#00bcd4'
+    name: "primary",
+    color: "#00bcd4",
   },
   {
-    name: 'info',
-    color: '#304CFD'
+    name: "info",
+    color: "#304CFD",
   },
   {
-    name: 'grey',
-    color: '#6c757d'
+    name: "grey",
+    color: "#6c757d",
   },
   {
-    name: 'red',
-    color: '#ef4d56'
+    name: "red",
+    color: "#ef4d56",
   },
   {
-    name: 'turquoise',
-    color: '#0dcaf0'
+    name: "turquoise",
+    color: "#0dcaf0",
   },
   {
-    name: 'blue',
-    color: '#425cbb'
+    name: "blue",
+    color: "#425cbb",
   },
   {
-    name: 'secondary',
-    color: '#7081b9'
+    name: "secondary",
+    color: "#7081b9",
   },
   {
-    name: 'pink',
-    color: '#f05492'
+    name: "pink",
+    color: "#f05492",
   },
   {
-    name: 'green',
-    color: '#69c380'
+    name: "green",
+    color: "#69c380",
   },
   {
-    name: 'yellow',
-    color: '#ffc835'
-  }
-]
+    name: "yellow",
+    color: "#ffc835",
+  },
+];
