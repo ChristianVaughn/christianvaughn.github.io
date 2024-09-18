58e36fdcfd0a27101bc189e03f384b88c861a01d
Christian Vaughn <me@christianvaughn.net>
2024-09-17 17:40:16 -0700

    Update resume

--- typescript/data/resume.ts
+++ typescript/data/resume.ts
@@ -20,14 +20,14 @@ export const educationHistory: IExperienceBox[] = [
 
 export const workingHistory: IExperienceBox[] = [
   {
-    text: "",
-    title: "Software Engineer II, Backend - Keiser Corporation",
-    year: "Aug 2023 - Apr 2024",
+    text: "Developing local fullstack applications utilizing SvelteKit and Typescript. Deployed a local inventory management system, utilizing modern Typescript and back-end frameworks (ElysiaJs), to provide a fully type safe system from the database to the front end.",
+    title: "Software Engineer, Fullstack - Computer Systems Plus",
+    year: "Apr 2024 - Current",
   },
   {
-    text: "Specializing in JavaScript and TypeScript, I've designed APIs and backend tools while implementing data-driven code generation solutions. My work leverages AWS and emphasizes agile collaboration for efficient product delivery and customer satisfaction.",
-    title: "Software Engineer, Backend - Keiser Corporation",
-    year: "Aug 2022 - Aug 2023",
+    text: "Specializing in JavaScript and TypeScript, I've designed APIs and backend tools while implementing data-driven code generation solutions. My work leverages AWS and emphasizes agile collaboration for efficient product delivery and customer satisfaction. I also engineered real time communication APIs to connect applications and strength training equipment",
+    title: "Software Engineer II, Backend - Keiser Corporation",
+    year: "Aug 2022 - Apr 2024",
   },
   {
     text: "I developed server automation software and designed a solution for processing auto repair estimates, which simplified ordering processes and cut manual tasks by 40%, while enhancing customer service resolution by 95%.",
